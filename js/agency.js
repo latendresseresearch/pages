02fcda6577cc957f51035a4f1fb2f04fbcd29fa6
Scott Kausler <kausy_63@hotmail.com>
2019-06-25 22:49:03 -0600

    First pass design

--- js/agency.js
+++ js/agency.js
@@ -15,28 +15,11 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
+  $(function(){
+    $("#header").load("header.html"); 
   });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 56
+  $(function(){
+    $("#footer").load("footer.html"); 
   });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
 })(jQuery); // End of use strict
