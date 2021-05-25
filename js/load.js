80656ed9db1c8fb703bfd6a981b3f592488ef30e
Scott Kausler <skausler@lovevery.com>
2021-05-25 12:03:52 -0600

    Redesign

---
+++ js/load.js
@@ -0,0 +1,12 @@
+(function($) {
+    "use strict"; // Start of use strict
+  
+    $(function(){
+      $("#header").load("header.html"); 
+    });
+  
+    $(function(){
+      $("#footer").load("footer.html"); 
+    });
+
+  })(jQuery); // End of use strict
